99edf9681a678fbfebbcbc35cd2be1c08949b202
Liviu Marian Mazilu <liviumazilu79@gmail.com>
2024-07-02 23:03:39 +0200

    Added mood handling

--- src/services/recommendation.service.ts
+++ src/services/recommendation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { RecommendationRequest } from 'src/DTOs/RecommendationRequest.dto';
+import { FilmApiResponseType } from 'src/types/FilmApiResponseType';
 import { ParsedFilmType } from 'src/types/ParsedFilm';
 import { RecommendationApiResponseType } from 'src/types/RecommendationApiResponseType';
 
@@ -135,43 +136,47 @@ Titolo4: "Avatar"`;
 Da qui il prompt: Raccomanda 4 ${request.contentType} che abbiano generi di  ${request.genre1} e ${request.genre2}, basati su questa risposta dell'umore: ${request.feeling}. Tieni a mente che all'user piace il film ${request.moviePreference}, quindi è possibile che gli aggradi qualcosa di simile.
 Il film deve anche incentrarsi in qualche modo (o con l'anno di uscita o che tratti di quegli anni) sugli anni ${request.years}
 Fornisci la risposta in formato JSON, avendo cura di separare i film tramite oggetto unico "films" con all'interno oggetti "film" contenenti il titolo, la descrizione, il cast, la durata e l'anno di uscita.
+Fornisci inoltre quello che può essere un mood in base alle risposte che ti vengono fornite.
+I possibili mood sono: Felice, Impegnato, Malinconico, Energico, Stanco, Creativo.
+Non fornire mai due film uguali.
 Esempio di risposta:
 {
+  "mood": "Creativo",
   "films": [
     {
       "film": {
-        "titolo": "Scary Movie",
-        "descrizione": "Una parodia dei famosi film horror degli anni '90 e primi 2000, che combina elementi comici esagerati con cliché horror.",
-        "cast": "Anna Faris, Jon Abrahams, Marlon Wayans, Regina Hall",
-        "durata": "88 minuti",
-        "anno": "2000"
-      }
-    },
-    {
-      "film": {
         "titolo": "Shaun of the Dead",
-        "descrizione": "Una commedia horror britannica che segue Shaun e il suo amico Ed mentre tentano di sopravvivere a un'apocalisse zombie.",
-        "cast": "Simon Pegg, Nick Frost, Kate Ashfield, Lucy Davis",
+        "descrizione": "Shaun è un commesso pigro che si ritrova nel mezzo di un'apocalisse zombie. Insieme al suo migliore amico, cerca di salvare la sua amata e la sua madre portandole al luogo più sicuro che conosce: il pub locale.",
+        "cast": "Simon Pegg, Nick Frost, Kate Ashfield",
         "durata": "99 minuti",
         "anno": "2004"
       }
     },
     {
       "film": {
-        "titolo": "Tucker & Dale vs. Evil",
-        "descrizione": "Un malinteso esilarante porta un gruppo di studenti universitari a credere che due gentili abitanti delle montagne siano assassini psicopatici.",
-        "cast": "Tyler Labine, Alan Tudyk, Katrina Bowden, Jesse Moss",
+        "titolo": "Tucker and Dale vs. Evil",
+        "descrizione": "Due amici, Tucker e Dale, vengono scambiati per maniaci assassini quando un gruppo di studenti universitari inizia a morire accidentalmente intorno alla loro baita in montagna.",
+        "cast": "Tyler Labine, Alan Tudyk, Katrina Bowden",
         "durata": "89 minuti",
         "anno": "2010"
       }
     },
     {
       "film": {
-        "titolo": "The Cabin in the Woods",
-        "descrizione": "Un moderno omaggio e satira dei film horror classici, dove cinque amici vanno in una remota cabina nei boschi e scoprono che nulla è come sembra.",
-        "cast": "Kristen Connolly, Chris Hemsworth, Anna Hutchison, Fran Kranz",
-        "durata": "95 minuti",
-        "anno": "2012"
+        "titolo": "What We Do in the Shadows",
+        "descrizione": "Un documentario fittizio sulle vite di quattro vampiri coinquilini che cercano di trovare il loro posto nella moderna vita notturna di Wellington.",
+        "cast": "Jemaine Clement, Taika Waititi, Jonathan Brugh",
+        "durata": "86 minuti",
+        "anno": "2014"
+      }
+    },
+    {
+      "film": {
+        "titolo": "Scary Movie",
+        "descrizione": "Una parodia dei film horror in cui un gruppo di amici affronta situazioni assurde e clownesche ispirate ai film del genere.",
+        "cast": "Anna Faris, Jon Abrahams, Marlon Wayans",
+        "durata": "88 minuti",
+        "anno": "2000"
       }
     }
   ]
@@ -179,58 +184,62 @@ Esempio di risposta:
   }
 
   /**
-   * Parses the response from the API and extracts the relevant information.
-   *
-   * @param {any} response - The response from the API.
-   * @return {object} An object containing the parsed information.
-   *                   The object has the following properties:
-   *                   - title: The title of the recommended content.
-   *                   - description: The description of the recommended content.
-   *                   - cast: The cast of the recommended content.
-   *                   - duration: The duration of the recommended content.
-   *                   - year: The year of release of the recommended content.
-   */
-  async parseFinalResponse(response: any): Promise<ParsedFilmType[]> {
-    let text = response.choices[0].message.content.trim();
+ * Parses the response from the API and extracts the relevant information.
+ *
+ * @param {any} response - The response from the API.
+ * @return {object} An object containing the parsed information.
+ *                   The object has the following properties:
+ *                   - mood: The mood related to the recommendation.
+ *                   - films: An array of objects containing recommended films.
+ *                             Each film object has the following properties:
+ *                             - title: The title of the recommended film.
+ *                             - description: The description of the recommended film.
+ *                             - cast: The cast of the recommended film.
+ *                             - duration: The duration of the recommended film.
+ *                             - year: The year of release of the recommended film.
+ *                             - url: The URL of the poster for the recommended film.
+ */
+ async parseFinalResponse(response: any): Promise<{ mood: string, films: ParsedFilmType[] }> {
+  let text = response.choices[0].message.content.trim();
 
-    // Remove the "```json" at the beginning and "```" at the end
-    if (text.startsWith('```json')) {
-      text = text.substring(7);
-    }
-    if (text.endsWith('```')) {
-      text = text.substring(0, text.length - 3);
-    }
-
-    // Parse the JSON text
-    const parsedResponse: RecommendationApiResponseType = JSON.parse(text);
-
-    const films: ParsedFilmType[] = await Promise.all(
-      parsedResponse.films.map(async (filmData) => {
-        console.log(filmData);
-        const film = filmData.film;
-        const titolo = film.titolo;
-        const descrizione = film.descrizione;
-        const cast = film.cast;
-        const durata = film.durata;
-        const annoDiUscita = film.anno;
-
-        // Fetch poster URL using OMDB API
-        const omdbResponse = await axios.get(
-          `https://omdbapi.com/?t=${titolo}&apikey=${process.env.OMDB_API_KEY}`,
-        );
-        const url = omdbResponse.data.Poster;
-
-        return {
-          title: titolo,
-          description: descrizione,
-          cast: cast,
-          duration: durata,
-          year: annoDiUscita,
-          url: url,
-        };
-      }),
-    );
-
-    return films;
+  // Remove the "```json" at the beginning and "```" at the end
+  if (text.startsWith('```json')) {
+    text = text.substring(7);
   }
+  if (text.endsWith('```')) {
+    text = text.substring(0, text.length - 3);
+  }
+
+  // Parse the JSON text
+  const parsedResponse = JSON.parse(text);
+
+  const mood = parsedResponse.mood;
+  const films: ParsedFilmType[] = await Promise.all(
+      parsedResponse.films.map(async (filmData: FilmApiResponseType) => {
+      const film = filmData.film;
+      const titolo = film.titolo;
+      const descrizione = film.descrizione;
+      const cast = film.cast;
+      const durata = film.durata;
+      const annoDiUscita = film.anno;
+
+      // Fetch poster URL using OMDB API
+      const omdbResponse = await axios.get(
+        `https://omdbapi.com/?t=${titolo}&apikey=${process.env.OMDB_API_KEY}`,
+      );
+      const url = omdbResponse.data.Poster;
+
+      return {
+        title: titolo,
+        description: descrizione,
+        cast: cast,
+        duration: durata,
+        year: annoDiUscita,
+        url: url,
+      };
+    }),
+  );
+
+  return { mood, films };
+}
 }
